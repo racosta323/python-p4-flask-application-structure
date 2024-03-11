430e5147a3954255e5ec8897ccf00a44fbecddb7
Rene <racosta323@gmail.com>
2024-03-11 14:15:23 -0500

    complete

--- server/app.py
+++ server/app.py
@@ -3,3 +3,18 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# app.route is an instance method that modifies app; allows us to define a route
+# registers index() to the applicationo instance
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+#anything in between <> will be passed to the app.route decorator as a parameter
+#add validity by using "string"
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
